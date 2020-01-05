b74d43a05fb0c12eb1d4ea3b80bc8cbe7b011e82
Brian Zerangue <brian.zerangue@gmail.com>
2020-01-05 15:11:11 -0600

    added mysql to path.zsh

--- path.zsh
+++ path.zsh
@@ -10,6 +10,9 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 # Use for Python
 export PATH="$HOME/Library/Python/3.8/bin:/usr/local/opt/python@3.8/bin:/usr/local/opt/python/libexec/bin:$PATH"
 
+# Use for MySQL 5.7
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
