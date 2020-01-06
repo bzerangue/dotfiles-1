fe934113212981628d09447643b8b7e55c9a1067
Brian Zerangue <brian.zerangue@gmail.com>
2020-01-06 08:45:04 -0600

    updated node path

--- path.zsh
+++ path.zsh
@@ -1,11 +1,15 @@
 # Load Composer tools
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 
+export N_PREFIX=$HOME/.n
+export PATH=$N_PREFIX/bin:$PATH
+
 # Load Node global installed binaries
-export PATH="$HOME/.node/bin:$PATH"
+export PATH="$HOME/.n/bin:$HOME/.node/bin:$PATH"
 
 # Use project specific binaries before global ones, see https://docs.python-guide.org/starting/install3/osx/
 export PATH="node_modules/.bin:vendor/bin:$PATH"
+
 
 # Use for Python
 export PATH="$HOME/Library/Python/3.8/bin:/usr/local/opt/python@3.8/bin:/usr/local/opt/python/libexec/bin:$PATH"
